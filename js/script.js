3b9739e40511367de800bcee78d033f21ae62dc0
francesca <fradeluca85@gmail.com>
2021-11-11 19:59:27 +0100

    correzione classi css e prima parte script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 // alert("ciao")
+
+const items = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+]; console.log(items);
+
+
+let fotografie = '';
+
+for (let i = 0; i < items.length; i++){
+    fotografie +=`
+    <div class="foto">
+        <img  src="${items[i]}" alt="">
+    </div>`
+}
+console.log(fotografie);
+
+const box  = document.querySelector(".box");
+box.innerHTML = fotografie
+
+const foto = document.querySelector ('.foto');
+foto.className = 'foto active';
